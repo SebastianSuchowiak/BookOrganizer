089036b80f4eb18992287da2a0cd07a606436a92
SebastianSuchowiak <suchowiak.sebastian@gmail.com>
2020-06-17 01:28:16 +0200

    Tags with colors and some styling

--- app/src/main/java/agh/wta/suchowiak/bookorganizer/BookViewAdapter.java
+++ app/src/main/java/agh/wta/suchowiak/bookorganizer/BookViewAdapter.java
@@ -1,43 +1,29 @@
 package agh.wta.suchowiak.bookorganizer;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import books.model.Book;
+import books.model.Tag;
 import co.lujun.androidtagview.TagContainerLayout;
+import co.lujun.androidtagview.TagView;
 import lombok.Getter;
-import lombok.Setter;
 
 public class BookViewAdapter extends RecyclerView.Adapter<BookViewAdapter.BookViewHolder> {
 
     private ArrayList<Book> books;
-
-    @Getter
-    static class BookViewHolder extends RecyclerView.ViewHolder {
-        private ImageView cover;
-        private TextView title;
-        private TextView author;
-        private TextView score;
-        private TagContainerLayout tags;
-
-        BookViewHolder(@NonNull View itemView) {
-            super(itemView);
-            cover = itemView.findViewById(R.id.cover);
-            title = itemView.findViewById(R.id.title);
-            author = itemView.findViewById(R.id.author);
-            score = itemView.findViewById(R.id.score);
-            tags = itemView.findViewById(R.id.tags);
-        }
-    }
 
     public BookViewAdapter(ArrayList<Book> books) {
         this.books = books;
@@ -58,11 +44,38 @@ public class BookViewAdapter extends RecyclerView.Adapter<BookViewAdapter.BookVi
         holder.getAuthor().setText(book.getAuthorsRep());
         holder.getTitle().setText(book.getTitle());
         holder.getScore().setText(book.getScore().toString());
-        book.getTags().forEach((tag) -> holder.getTags().addTag(tag.getName()));
+
+        List<int[]> colors = book.getTags()
+                .stream()
+                .map((tag) -> {
+                    int mainColor = Color.parseColor(tag.getColor());
+                    return new int[]{mainColor, mainColor, Color.BLACK, mainColor};
+                })
+                .collect(Collectors.toList());
+        List<String> tags = book.getTags().stream().map(Tag::getName).collect(Collectors.toList());
+        holder.getTags().setTags(tags, colors);
     }
 
     @Override
     public int getItemCount() {
         return books.size();
+    }
+
+    @Getter
+    static class BookViewHolder extends RecyclerView.ViewHolder {
+        private ImageView cover;
+        private TextView title;
+        private TextView author;
+        private TextView score;
+        private TagContainerLayout tags;
+
+        BookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cover = itemView.findViewById(R.id.cover);
+            title = itemView.findViewById(R.id.title);
+            author = itemView.findViewById(R.id.author);
+            score = itemView.findViewById(R.id.score);
+            tags = itemView.findViewById(R.id.tags);
+        }
     }
 }
--- app/src/main/java/agh/wta/suchowiak/bookorganizer/TagsFragment.java
+++ app/src/main/java/agh/wta/suchowiak/bookorganizer/TagsFragment.java
@@ -11,7 +11,6 @@ public class TagsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tags_fragment, container, false);
     }
 }
