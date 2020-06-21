d313b1bd150debd94d24b4a44f3ec44a8f02d0fb
SebastianSuchowiak <suchowiak.sebastian@gmail.com>
2020-06-21 19:39:08 +0200

    Added tags to new book

--- app/src/main/java/agh/wta/suchowiak/bookorganizer/NewBookActivity.java
+++ app/src/main/java/agh/wta/suchowiak/bookorganizer/NewBookActivity.java
@@ -10,11 +10,14 @@ import android.widget.RatingBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import books.model.Book;
 import books.model.Status;
+import books.model.Tag;
 import books.model.User;
 import books.repository.BookRepository;
 import butterknife.BindView;
@@ -54,6 +57,7 @@ public class NewBookActivity extends AppCompatActivity {
         button.setOnClickListener(this::submit);
 
         newBook = new Book();
+        newBook.setTags(new ArrayList<>());
     }
 
     public void onRadioButtonClicked(View view) {
@@ -75,6 +79,65 @@ public class NewBookActivity extends AppCompatActivity {
         }
     }
 
+    //TODO: Very ugly to be replaced by something dynamic
+    public void onRadioButtonTagClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.tag_long:
+                if (checked)
+                    newBook.getTags().add(tags.get(0));
+                else {
+                    newBook.getTags().remove(tags.get(0));
+                }
+                break;
+            case R.id.tag_short:
+                if (checked)
+                    newBook.getTags().add(tags.get(1));
+                else {
+                    newBook.getTags().remove(tags.get(1));
+                }
+                break;
+            case R.id.tag_fantasy:
+                if (checked)
+                    newBook.getTags().add(tags.get(2));
+                else {
+                    newBook.getTags().remove(tags.get(2));
+                }
+                break;
+            case R.id.tag_horror:
+                if (checked)
+                    newBook.getTags().add(tags.get(3));
+                else {
+                    newBook.getTags().remove(tags.get(3));
+                }
+                break;
+            case R.id.tag_for_kids:
+                if (checked)
+                    newBook.getTags().add(tags.get(4));
+                else {
+                    newBook.getTags().remove(tags.get(4));
+                }
+                break;
+            case R.id.tag_the_best:
+                if (checked)
+                    newBook.getTags().add(tags.get(5));
+                else {
+                    newBook.getTags().remove(tags.get(5));
+                }
+                break;
+        }
+    }
+
+    List<Tag> tags = Arrays.asList(
+            new Tag("long", "#22222b"),
+            new Tag("short", "#34ebeb"),
+            new Tag("fantasy", "#7f75c7"),
+            new Tag("horror", "#a2332b"),
+            new Tag("for kids", "#34eb46"),
+            new Tag("the best", "#e3716d")
+            );
+
     private void submit(View view) {
         if (isValid()) {
             List<String> authors = new ArrayList<>();
@@ -90,6 +153,7 @@ public class NewBookActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             intent.putExtra("user", user);
             startActivity(intent);
+            finish();
         }
     }
 
