98d10253acac6e67dc8991035bac98cd5f8c37cf
SebastianSuchowiak <suchowiak.sebastian@gmail.com>
2020-06-21 15:38:18 +0200

    Update new book activity

---
+++ app/src/main/java/agh/wta/suchowiak/bookorganizer/NewBookActivity.java
@@ -0,0 +1,18 @@
+package agh.wta.suchowiak.bookorganizer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class NewBookFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+    }
+}
